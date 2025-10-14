29bf6df2815cc86a7e760811d6cbacac3b51d065
nunusha712 <noa.hadad712@gmail.com>
2025-10-14 16:58:52 +0300

    removed fields from log model

--- models/Log.js
+++ models/Log.js
@@ -3,11 +3,10 @@ const mongoose = require("mongoose");
 const LogSchema = new mongoose.Schema({
   level: { type: String, enum: ["INFO", "WARN", "ERROR"], default: "INFO" },
   message: { type: String, required: true },
-  service: { type: String }, // e.g. "streaming-api", "auth-service"
+  service: { type: String }, 
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   profileId: { type: String },
   timestamp: { type: Date, default: Date.now },
-  details: { type: Object } // optional field for stack traces, payloads, etc.
 });
 
 LogSchema.index({ level: 1, timestamp: -1 });
