632565d010a033640917e1fe747381ea35cdfc7b
nunusha712 <noa.hadad712@gmail.com>
2025-10-22 22:07:16 +0300

    full profile editing functionality

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -9,17 +9,39 @@ async function updateProfile(req, res) {
     const profileId = req.params.profileId;
     const newName = req.body.displayName;
     const { action } = req.body;
-    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!" , req.params, "!!!!!!!!!!!!!!!!!!!!!!")
 
     // Handle different form actions
     if (action === "update_name") {
-        console.log("Updating profile name to:", newName);
         const user = await User.findOneAndUpdate(
             { _id: userId, "profiles._id": profileId },
             { $set: { "profiles.$.name": newName } },
             { new: true } // return updated doc
         );}
+    else if (action === "update_preferences") {
+        console.log("Updating profile preferences" , req.body.preferences);
+        // Make sure preferences is always an array
+    let preferences = req.body.preferences;
+    if (!Array.isArray(preferences)) {
+        preferences = preferences ? [preferences] : [];
+    }
 
+    // Update the nested profile inside the user
+    await User.findOneAndUpdate(
+        { _id: req.session.userId, "profiles._id": profileId },
+        { $set: { "profiles.$.genrePreferences": preferences } },
+        { new: true }
+    );
+
+} else if (action === "update_picture") {
+    const { newPicture } = req.body;
+
+    // Update the picture in the nested profile
+    await User.findOneAndUpdate(
+        { _id: req.session.userId, "profiles._id": profileId },
+        { $set: { "profiles.$.picture": newPicture } },
+        { new: true }
+    );
+}
   } catch (err) {
     console.error("Error updating profile name:", err);
     throw err;
