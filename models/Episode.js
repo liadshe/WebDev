cd556bd8e67b27484175b871039b4fa39401136c
liadshe <liadshe1@gmail.com>
2025-11-07 11:32:38 +0200

    add episode model

---
+++ models/Episode.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const EpisodeSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  seasonNumber: { type: Number, required: true },
+  episodeNumber: { type: Number, required: true },
+  releaseDate: { type: Date },
+  durationSeconds: { type: Number },
+  videoPath: { type: String },
+  coverImagePath: { type: String },
+
+// Relationship: each episode belongs to a series
+  series: { type: mongoose.Schema.Types.ObjectId, ref: "Content", required: true }});
+
+// create a unique index to prevent duplicate episodes in the same season of a series
+EpisodeSchema.index({ series: 1, seasonNumber: 1, episodeNumber: 1 }, { unique: true });
+
+module.exports = mongoose.models.Episode || mongoose.model("Episode", EpisodeSchema);
