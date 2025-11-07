960bbe26a334139db817982ba3a6ccde079a4965
liadshe <liadshe1@gmail.com>
2025-11-07 11:28:27 +0200

    add type for content and seasons number for series

--- models/Content.js
+++ models/Content.js
@@ -3,16 +3,25 @@ const mongoose = require("mongoose");
 const ContentSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
+  type: { type: String,
+          enum: ["movie", "series"],
+          required: true,
+          index: true
+        },
   genre: [{ type: String, index: true }],
   cast: [{ type: String, index: true }],
   director: { type: String },
   releaseYear: { type: Number },
-  durationMinutes: { type: Number },
-  durationSeconds: { type: Number },
   rating: { type: String }, // from IMDB or similar
   videoPath: { type: String },
   coverImagePath: { type: String },
-});
+
+  // for movies only 
+  durationSeconds: { type: Number },
+  
+  // for series only    
+  seasonNumber: { type: Number, default: 1 }
+}); 
 
 module.exports =
   mongoose.models.Content || mongoose.model("Content", ContentSchema);
